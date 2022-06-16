efee73d44a1eec098f76737a314a7afba02df810
yusaincedere <yusaincedere@gmail.com>
2022-06-16 15:56:53 +0300

    version 2

--- src/components/QrScannerComponent.jsx
+++ src/components/QrScannerComponent.jsx
@@ -1,40 +1,41 @@
-import React, { Component } from "react";
-import QrReader from "react-qr-scanner";
+import React, { Component, useState } from "react";
 
-class QrScannerComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      delay: 100,
-      result: "No result",
-    };
-    this.handleScan = this.handleScan.bind(this);
-  }
-  handleScan(data) {
-    this.setState({
-      result: data,
-    });
-  }
-  handleError(err) {
-    console.error(err);
-  }
-  render() {
-    const previewStyle = {
-      height: 240,
-      width: 320,
-    };
+import { QrReader } from "react-qr-reader";
 
-    return (
-      <div className="container" style={{ display: this.props.display }}>
+const QrScannerComponent = (props) => {
+  const [data, setData] = useState("No result");
+
+  const openScanner = () => {
+    if (props.permission === true) {
+      console.log("Cam ON");
+      return (
         <QrReader
-          delay={this.state.delay}
-          style={previewStyle}
-          onError={this.handleError}
-          onScan={this.handleScan}
+          onResult={(result, error) => {
+            if (!!result) {
+              setData(result?.text);
+            }
+
+            if (!!error) {
+              console.info(error);
+            }
+          }}
+          videoStyle={{ width: 400, height: 300 }}
+          containerStyle={{ width: 400, height: 300 }}
+          constraints={{ facingMode: "user" }}
+          scanDelay="100"
         />
-        <p>{this.state.result}</p>
-      </div>
-    );
-  }
-}
+      );
+    } else {
+      console.log("Cam OFF");
+      return null;
+    }
+  };
+
+  return (
+    <div className="col  text-center" style={{ display: props.display }}>
+      <div className="row justify-content-md-center">{openScanner()}</div>
+      <p>{data}</p>
+    </div>
+  );
+};
 export default QrScannerComponent;
